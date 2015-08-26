15c89262ebf52b5e81ed85acb405e886c1c58336
Jorge Castro <jacastrom@gmail.com>
2015-08-26 21:38:55 +0200

    event class added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823152736) do
+ActiveRecord::Schema.define(version: 20150826192441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,5 +48,10 @@ ActiveRecord::Schema.define(version: 20150823152736) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.text   "description"
+  end
 
 end
